497d339cd9167403d684b6392c6db4996d340579
JavaKappa <nextnation7@gmail.com>
2020-01-02 23:48:21 +0300

    Optimization and implement HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -7,11 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.time.temporal.TemporalAmount;
-import java.time.temporal.TemporalUnit;
 import java.util.*;
-import java.util.function.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
@@ -22,14 +18,12 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 12, 0), "Завтрак тост", -101),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 12, 0), "Завтрак тост", 101),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
-
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-//        mealsTo.forEach(System.out::println);
-
+        mealsTo.forEach(System.out::println);
         System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
@@ -51,30 +45,25 @@ public class UserMealsUtil {
                 dayAndCalories.put(day, m.getCalories());
             }
         });
-        //заполняем всеми блюдами со значением excess
+        //Filter List<UserMeal> on valid time
+        //Don't forget ask about if we take argument in method can we change him or not! How is right in production?
+        List<UserMeal> mealsClone = new ArrayList<>(meals);
+        mealsClone.removeIf(userMealWithExcess -> !TimeUtil.isBetweenInclusive(userMealWithExcess.getDateTime().toLocalTime(), startTime, endTime));
         List<UserMealWithExcess> userMealWithExcesses = new ArrayList<>();
-        meals.forEach(m -> {
-            if (dayAndCalories.get(m.getDateTime().toLocalDate()) > caloriesPerDay) {
-                userMealWithExcesses.add(new UserMealWithExcess(m, true));
-            } else {
-                userMealWithExcesses.add(new UserMealWithExcess(m, false));
-            }
-        });
-
-        //фильтруем
-        userMealWithExcesses.removeIf(userMealWithExcess -> !TimeUtil.isBetweenInclusive(userMealWithExcess.getDateTime().toLocalTime(), startTime, endTime));
-
+        mealsClone.forEach(m -> userMealWithExcesses.add(dayAndCalories.get(m.getDateTime().toLocalDate()) > caloriesPerDay ? new UserMealWithExcess(m, true) : new UserMealWithExcess(m, false)));
         return userMealWithExcesses;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         System.out.println("TODO Implement by streams");
         //get UserMeals per day
-        Map<LocalDate, Integer> dayAndCalories = meals.stream().collect(Collectors.toMap(x-> x.getDateTime().toLocalDate(), UserMeal::getCalories, Integer::sum));
-        //now convert UserMeals list to UserMealsWithExcess
-
-        System.out.println(dayAndCalories);
-        return null;
+        Map<LocalDate, Integer> dayAndCalories = meals.stream().collect(Collectors.toMap(x -> x.getDateTime().toLocalDate(), UserMeal::getCalories, Integer::sum));
+        //now we filter on LocalTime value, then convert UserMeals list to UserMealsWithExcess
+        return meals.stream()
+                .filter(meal -> TimeUtil.isBetweenInclusive(meal.getDateTime().toLocalTime(), startTime, endTime))
+                .map(meal -> {
+                    LocalDate date = meal.getDateTime().toLocalDate();
+                    return new UserMealWithExcess(meal, dayAndCalories.get(date) > caloriesPerDay);
+                }).collect(Collectors.toList());
     }
-
 }
