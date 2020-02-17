34ca4fcc8807b49e9ca7bfb24ae492c7ecccc954
JavaKappa <nextnation7@gmail.com>
2020-02-17 21:09:47 +0300

    Add Date andtimes variables to realize filter logic

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,8 +1,57 @@
 package ru.javawebinar.topjava.web;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
+
+    private static int caloriesPerDay = DEFAULT_CALORIES_PER_DAY;
+    private static LocalDate startDate = LocalDate.MIN;
+    private static LocalDate endDate = LocalDate.MAX;
+    private static LocalTime startTime = LocalTime.MIN;
+    private static LocalTime endTime = LocalTime.MAX;
+
+    public static int getCaloriesPerDay() {
+        return caloriesPerDay;
+    }
+
+    public static void setCaloriesPerDay(int caloriesPerDay) {
+        SecurityUtil.caloriesPerDay = caloriesPerDay;
+    }
+
+    public static LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public static void setStartDate(LocalDate startDate) {
+        SecurityUtil.startDate = startDate;
+    }
+
+    public static LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public static void setEndDate(LocalDate endDate) {
+        SecurityUtil.endDate = endDate;
+    }
+
+    public static LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public static void setStartTime(LocalTime startTime) {
+        SecurityUtil.startTime = startTime;
+    }
+
+    public static LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public static void setEndTime(LocalTime endTime) {
+        SecurityUtil.endTime = endTime;
+    }
 
     public static int authUserId() {
         return 1;
@@ -11,4 +60,6 @@ public class SecurityUtil {
     public static int authUserCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
     }
+
+
 }
