cb8f7d0c406d33321aadcb7509ff74a0305ab97b
JavaKappa <nextnation7@gmail.com>
2020-02-23 22:57:57 +0300

    Implements initDB Implement populateDB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,9 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(description, calories, user_id) VALUES
+('user_Meal 1', 500, 100000),
+('user_Meal 2', 750, 100000),
+('admin_Meal 1', 500, 100001),
+('admin_Meal 2', 123, 100001);
